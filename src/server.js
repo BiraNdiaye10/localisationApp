2b47848ab33732f3fb8de3fe831a13f06130b29b
birandiaye10 <nbira38@gmail.com>
2022-01-13 14:18:12 +0000

    deploy 1

--- src/server.js
+++ src/server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/Localisation'));
+app.use(express.static(__dirname + '/dist/Landio'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/Localisation/index.html'));
+res.sendFile(path.join(__dirname+'/dist/Landio/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
